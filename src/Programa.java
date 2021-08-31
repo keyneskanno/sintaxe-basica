96c6ee90490c1f9a0d81bac46e15c0e61f5d3fa3
Keynes Kanno <keyneskanno@gmail.com>
2021-08-31 15:26:20 -0300

    Estudando tipo flutuante

--- src/Programa.java
+++ src/Programa.java
@@ -7,17 +7,17 @@ public class Programa {
 		int idade;
 		idade = 37;
 		
-		System.out.println(idade);
+		//System.out.println(idade);
 		
 		idade = 30+10;
 		
-		System.out.println(idade);
+		//System.out.println(idade);
 		
 		idade = 7 * 5 + 2;
 		
-		System.out.println(idade);
+		//System.out.println(idade);
 		
-		System.out.println("a idade é " + idade + ", parabéns! ");
+		//System.out.println("a idade é " + idade + ", parabéns! ");
 		
 		//git status
 		//git commit -m "Imprimindo tipo inteiro"
@@ -25,6 +25,27 @@ public class Programa {
 		//git log 
 		//git remote -v
 		//git push -u origin master
+		//git tag -a v1.1 -m "versão 1.2 imprimindo tipo inteiro"
+		//git push -u origin v1.1
+		
+		double salario;
+		salario = 1250.70;
+		System.out.println("meu salário é "+ salario);
+		
+		double divisao = 3.14/2;
+		System.out.println(divisao);
+		
+		int outraDivisao = 5/2;
+		System.out.println(outraDivisao);
+		
+		double novaTentativa = 5.0/2;
+		System.out.println(novaTentativa);
+		
+		 
+		
+		
+		
+		
 		
 	}
 	
