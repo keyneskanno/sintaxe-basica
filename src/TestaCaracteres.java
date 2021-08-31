926999506f5af4e0e3b35caebfb9019cb2b8f52c
Keynes Kanno <keyneskanno@gmail.com>
2021-08-31 16:11:19 -0300

    Estudando tipos char e String

---
+++ src/TestaCaracteres.java
@@ -0,0 +1,25 @@
+
+public class TestaCaracteres {
+	
+	public static void main(String[] args) {
+		//char guarda uma única letra
+		char letra = 'a';
+		System.out.println(letra);
+		
+		
+		char valor = 65; //tabela ASC
+		System.out.println(valor); //imprime a letra A
+		
+		valor = (char)(valor + 1); 
+		System.out.println(valor); //imprime o B
+		
+		String palavra = "alura cursos online de tecnologia"; //String é um objeto
+		System.out.println(palavra);
+		
+		palavra = palavra + 2020;
+		System.out.println(palavra);
+	
+		
+	}
+
+}
