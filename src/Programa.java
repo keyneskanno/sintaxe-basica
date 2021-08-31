3eaefc53d0bbf9613320da215d3fa47a45092ad2
Keynes Kanno <keyneskanno@gmail.com>
2021-08-31 15:06:22 -0300

    Imprimindo tipo inteiro

--- src/Programa.java
+++ src/Programa.java
@@ -2,7 +2,23 @@
 public class Programa {
 	
 	public static void main(String[] args) {
-		System.out.println("Olá mundo");
+		System.out.println("Olá novo teste");
+		
+		int idade;
+		idade = 37;
+		
+		System.out.println(idade);
+		
+		idade = 30+10;
+		
+		System.out.println(idade);
+		
+		idade = 7 * 5 + 2;
+		
+		System.out.println(idade);
+		
+		System.out.println("a idade é " + idade + ", parabéns! ");
+		
 	}
 	
 }
