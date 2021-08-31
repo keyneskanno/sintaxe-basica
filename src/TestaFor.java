83f758e4993338c625e036f59b5679c58d9aa98c
Keynes Kanno <keyneskanno@gmail.com>
2021-08-31 19:54:11 -0300

    Exercicios de lacos for

---
+++ src/TestaFor.java
@@ -0,0 +1,15 @@
+import javax.swing.plaf.synth.SynthSeparatorUI;
+
+public class TestaFor {
+	public static void main(String[] args) {
+
+		int contador = 0;
+		int total = 0;
+		for (contador = 0; contador <= 10; contador++) {
+			System.out.println(contador);
+
+		}
+
+		System.out.println(contador);
+	}
+}
